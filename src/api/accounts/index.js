2d9dccb90f10d6258581259d613fb349023ebcf5
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-03 06:25:35 +0700

    edit routes foods, home and account

--- src/api/accounts/index.js
+++ src/api/accounts/index.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./controller")
-const helpers = require("../../helpers")
+const controller = require("./controller");
+const helpers = require("../../helpers");
 
-router.post("/register", controller.register)
-router.post("/login", controller.login)
-router.put("/logout", helpers.isAuthenticated, controller.logout)
+router.post("/register", controller.register);
+router.post("/login", controller.login);
+router.put("/logout", controller.logout);
 
-router.get("/", helpers.isAuthenticated, controller.get)
-router.get("/bypass", controller.getBypass)
-router.get("/:id", controller.getById)
+router.get("/", controller.get);
+router.get("/bypass", controller.getBypass);
+router.get("/:id", controller.getById);
 
 // router.get("/review_history/:id", controller.getReviewHistory)
 
-router.post("/get_user_detail", helpers.isAuthenticated, controller.getUserDetail)
+router.post("/get_user_detail", controller.getUserDetail);
 
 module.exports = router;
-
--- src/api/foods/index.js
+++ src/api/foods/index.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const controller = require("./controller")
+const controller = require("./controller");
 
-const helpers = require("../../helpers")
+// const helpers = require("../../helpers")
 
-router.get("/", controller.get)
-router.get("/:id", controller.getById)
+router.get("/", controller.get);
+router.get("/:id", controller.getById);
 
-router.post("/", helpers.isAuthenticated, controller.post)
+router.post("/", controller.post);
 
-router.delete("/", helpers.isAuthenticated, controller.delete)
-router.delete("/:id", helpers.isAuthenticated, controller.deleteById)
+router.delete("/", controller.delete);
+router.delete("/:id", controller.deleteById);
 
-router.put("/:id", helpers.isAuthenticated, controller.putById)
-router.post("/add_review/:id", helpers.isAuthenticated, controller.addReviewById)
+router.put("/:id", controller.putById);
+router.post("/add_review/:id", controller.addReviewById);
 
-router.get("/review_history/:id", controller.getReviewHistory)
-router.get("/get_cospaces_by_user/:id", controller.getFoodByUser)
+router.get("/review_history/:id", controller.getReviewHistory);
+router.get("/get_cospaces_by_user/:id", controller.getFoodByUser);
 
-module.exports = router
+module.exports = router;
