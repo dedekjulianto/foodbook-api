a8ca5df748eb2e56e1bca41b8343fa3ecb3c49bc
Dedek Julianto <dedekproject@gmail.com>
2018-05-03 11:39:12 +0700

    edit accounts API 2

--- src/api/accounts/index.js
+++ src/api/accounts/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./controller");
-const helpers = require("../../helpers");
 
-router.post("/register", controller.register);
-router.post("/login", controller.login);
-router.put("/logout", controller.logout);
+const controller = require("./controller")
+// const helpers = require("../../helpers")
 
-router.get("/", controller.get);
-router.get("/bypass", controller.getBypass);
-router.get("/:id", controller.getById);
+router.post("/register", controller.register)
+router.post("/login", controller.login)
+router.put("/logout", controller.logout)
+
+router.get("/", controller.get)
+router.get("/bypass", controller.getBypass)
+router.get("/:id", controller.getById)
 
 // router.get("/review_history/:id", controller.getReviewHistory)
 
-router.post("/get_user_detail", controller.getUserDetail);
+router.post("/get_user_detail", controller.getUserDetail)
 
 module.exports = router;
