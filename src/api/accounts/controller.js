9633787fcd18f231daaaad561dcffe69dec03fc0
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-07 15:27:58 +0700

    Change data type of array in foods model

--- src/api/accounts/controller.js
+++ src/api/accounts/controller.js
@@ -1,7 +1,7 @@
 // const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const Account = require("./model")
+const Account = require("./model");
 
 // const helpers = require("../../helpers")
 
@@ -16,45 +16,47 @@ module.exports = {
       .exec((err, accounts) => {
         res.send({
           data: accounts
-        })
-      })
+        });
+      });
   },
   // ---------------------------------------------------------------------------
 
   // GET /accounts/:id
   getById: (req, res) => {
-    Account.findOne({id: Number(req.params.id)}, (err, account) => {
+    Account.findOne({ id: Number(req.params.id) }, (err, account) => {
       res.send({
         params: req.params,
         data: account
-      })
-    })
+      });
+    });
   },
   // ---------------------------------------------------------------------------
 
   // POST /accounts
   register: (req, res) => {
-    Account.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      rePassword: req.body.rePassword
-
-    }, (err, resource) => {
-      if (err) return handleError(err);
-      res.send({message: "succes register", data: resource});
-    })
+    Account.create(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        rePassword: req.body.rePassword
+      },
+      (err, resource) => {
+        if (err) res.status(400).json({ error: err });
+        res.status(200).send({ message: "succes register", data: resource });
+      }
+    );
   },
   // ---------------------------------------------------------------------------
 
   // DELETE /accounts
   delete: (req, res) => {
-    Account.remove({}, (error) => {
-      if (error) res.status(400).json({error: error})
-      res.status(200).send({message: "All accounts have been removed."})
-    })
+    Account.remove({}, error => {
+      if (error) res.status(400).json({ error: error });
+      res.status(200).send({ message: "All accounts have been removed." });
+    });
   },
   // ---------------------------------------------------------------------------
 
@@ -106,12 +108,9 @@ module.exports = {
 
   // ---------------------------------------------------------------------------
 
-
-
-
   // ---------------------------------------------------------------------------
   // POST /accounts/login
-/*
+  /*
 login: (req, res) => {
     // Create body object
     const body = {
@@ -119,79 +118,79 @@ login: (req, res) => {
       password: req.body.password
     }
 */
-    // Find one account by email
-    // Account.findOne({email: body.email})
-    //   .then((account) => {
-    //     console.log(account);
-    //     const validPassword = bcrypt.compareSync(
-    //       body.password,
-    //       account.password
-    //     )
+  // Find one account by email
+  // Account.findOne({email: body.email})
+  //   .then((account) => {
+  //     console.log(account);
+  //     const validPassword = bcrypt.compareSync(
+  //       body.password,
+  //       account.password
+  //     )
 
-        // console.log(validPassword)
+  // console.log(validPassword)
 
-        // console.log(">>> account found:", account)
-        // console.log({ validPassword })
+  // console.log(">>> account found:", account)
+  // console.log({ validPassword })
 
-        // if (!account) {
-        //   // (1) If account is not found
-        //   res.send({
-        //     message: `Login failed because account with email '${
-        //       body.email
-        //     }' is not found`
-        //   })
-        // } else if (!validPassword) {
-        //   // (2) If the found account is logged in with unmatched password
-        //   res.send({
-        //     message: `Sign in failed because password of '${username}' is not match.`
-        //   })
-        // } else {
-          // (3) If the found account is matched with the password
-          // console.log({ account })
+  // if (!account) {
+  //   // (1) If account is not found
+  //   res.send({
+  //     message: `Login failed because account with email '${
+  //       body.email
+  //     }' is not found`
+  //   })
+  // } else if (!validPassword) {
+  //   // (2) If the found account is logged in with unmatched password
+  //   res.send({
+  //     message: `Sign in failed because password of '${username}' is not match.`
+  //   })
+  // } else {
+  // (3) If the found account is matched with the password
+  // console.log({ account })
 
-          // (4) Create token content and config
-          // let content = {
-          //   payload: {
-          //     // or claims
-          //     iss: process.env.URL, // ISSUER: DOMAIN/URL of the service
-          //     sub: account._id, // SUBJECT: OID/UID/UUID/GUID
-          //     id: account.id, // ACCOUNTID: Sequential ID
-          //     name: account.name, // NAME: Full name
-          //     email: account.email // EMAIL: Email address
-          //   },
-          //   secret: process.env.JWT_SECRET,
-          //   options: {
-          //     expiresIn: "30d" // EXPIRATION: 30 days
-          //   }
-          // }
-          // console.log({ content })
+  // (4) Create token content and config
+  // let content = {
+  //   payload: {
+  //     // or claims
+  //     iss: process.env.URL, // ISSUER: DOMAIN/URL of the service
+  //     sub: account._id, // SUBJECT: OID/UID/UUID/GUID
+  //     id: account.id, // ACCOUNTID: Sequential ID
+  //     name: account.name, // NAME: Full name
+  //     email: account.email // EMAIL: Email address
+  //   },
+  //   secret: process.env.JWT_SECRET,
+  //   options: {
+  //     expiresIn: "30d" // EXPIRATION: 30 days
+  //   }
+  // }
+  // console.log({ content })
 
-          // (5) Generate a token
-      //     const token = helpers.generateJWT(content)
-      //     // console.log({ token })
-      //
-      //     // (6) Set logged in status
-      //     helpers.setLoggedIn(account, true)
-      //     // console.log(account.name
-      //     // (7) Finally send that token
-      //     res.send({
-      //       message: "You are logged in",
-      //       email: body.email,
-      //       name: account.name,
-      //       profile_picture: account.profile_picture,
-      //       id: account.id,
-      //       token: token
-      //     })
-      //   }
-      // })
-      // .catch((err) => {
-      //   // If there's an error while finding the account
-      //   if (err)
-      //     res.send({
-      //       message: `Something went wrong when try to logging in`
-      //     })
-      // })
-    // Finished sign in
+  // (5) Generate a token
+  //     const token = helpers.generateJWT(content)
+  //     // console.log({ token })
+  //
+  //     // (6) Set logged in status
+  //     helpers.setLoggedIn(account, true)
+  //     // console.log(account.name
+  //     // (7) Finally send that token
+  //     res.send({
+  //       message: "You are logged in",
+  //       email: body.email,
+  //       name: account.name,
+  //       profile_picture: account.profile_picture,
+  //       id: account.id,
+  //       token: token
+  //     })
+  //   }
+  // })
+  // .catch((err) => {
+  //   // If there's an error while finding the account
+  //   if (err)
+  //     res.send({
+  //       message: `Something went wrong when try to logging in`
+  //     })
+  // })
+  // Finished sign in
   // },
 
   // ---------------------------------------------------------------------------
@@ -211,28 +210,27 @@ login: (req, res) => {
   // ---------------------------------------------------------------------------
   // GET /accounts
   getReviewHistory: (req, res) => {
-
-    Account.find({reviews: {$elemMatch: {_account: "5a8d300c4cf01a3ee36818a1"}}})
+    Account.find({
+      reviews: { $elemMatch: { _account: "5a8d300c4cf01a3ee36818a1" } }
+    })
       .populate({
         path: "posts"
       })
       .exec((err, accounts) => {
         res.send({
           data: accounts
-        })
-      })
+        });
+      });
   },
 
   // ---------------------------------------------------------------------------
   // GET /accounts/get_user_detail
   getUserDetail: (req, res) => {
     // res.send(req.decoded.id)
-    Account.find({id: req.decoded.id})
-      .exec((err, accounts) => {
-        res.send({
-          data: accounts
-        })
-      })
-  },
-
-}
+    Account.find({ id: req.decoded.id }).exec((err, accounts) => {
+      res.send({
+        data: accounts
+      });
+    });
+  }
+};
--- src/api/foods/model.js
+++ src/api/foods/model.js
@@ -9,25 +9,25 @@ const modelName = "foods";
 
 // SCHEMA ----------------------------------------------------------------------
 
-const schema = new Schema({
-  // _account: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "Account"
-  // },
-  name: {
-    type: String
-  },
-  overview: {
-    type: String,
-    default: ""
-  },
-  price: {
-    type: Number,
-    default: ""
-  },
-  photos: [String],
-  address:
-    {
+const schema = new Schema(
+  {
+    // _account: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "Account"
+    // },
+    name: {
+      type: String
+    },
+    overview: {
+      type: String,
+      default: ""
+    },
+    price: {
+      type: Number,
+      default: ""
+    },
+    photos: [],
+    address: {
       street: {
         type: String,
         default: ""
@@ -37,58 +37,63 @@ const schema = new Schema({
         default: ""
       }
     },
-  coordinate: {
-    latitude: {
-      type: Number,
-      default: 0
-    },
-    longitude: {
-      type: Number,
-      default: 0
-    }
-  },
-  reviews: [
-    {
-      // _account: {
-      //   type: Schema.Types.ObjectId,
-      //   ref: "Account"
-      // },
-      comment: {
-        type: String
-      },
-      rating: {
+    coordinate: {
+      latitude: {
         type: Number,
-        enum: [1, 2, 3, 4, 5]
+        default: 0
+      },
+      longitude: {
+        type: Number,
+        default: 0
       }
-    }
-  ]
-}, {timestamps: true})
+    },
+    reviews: [
+      {
+        // _account: {
+        //   type: Schema.Types.ObjectId,
+        //   ref: "Account"
+        // },
+        comment: {
+          type: String
+        },
+        rating: {
+          type: Number,
+          enum: [1, 2, 3, 4, 5]
+        }
+      }
+    ]
+  },
+  { timestamps: true }
+);
 
 // GENERATED FIELDS ------------------------------------------------------------
 
 schema.plugin(sequence, {
   id: "food_counter",
   inc_field: "id"
-})
+});
 
 // DATA POPULATION -------------------------------------------------------------
 
 schema.pre("find", function(next) {
-  next()
-})
+  next();
+});
 
 schema.pre("findOne", function(next) {
-  next()
-})
+  next();
+});
 
 schema.pre("update", function() {
-  this.update({}, {
-    $set: {
-      updatedAt: new Date()
+  this.update(
+    {},
+    {
+      $set: {
+        updatedAt: new Date()
+      }
     }
-  })
-})
+  );
+});
 
 // REGISTER THE SCHEMA INTO MODEL ----------------------------------------------
 
-module.exports = mongoose.model(modelName, schema)
+module.exports = mongoose.model(modelName, schema);
