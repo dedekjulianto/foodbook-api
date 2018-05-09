586d77baba2ea22817dbdc7a91ae9b39d7e6d1d3
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-09 18:15:20 +0700

    Fix review history

--- src/api/foods/index.js
+++ src/api/foods/index.js
@@ -7,7 +7,7 @@ router.post("/", helpers.isAuthenticated, controller.post);
 router.get("/", controller.get);
 router.get("/search", controller.getByQuery);
 router.get("/:id", controller.getById);
-// router.get("/review_history/:id", controller.getReviewHistory);
+router.get("/review_history/:id", controller.getReviewHistory);
 router.get("/get_Food_by_user/:id", controller.getFoodByUser);
 router.put("/:id", helpers.isAuthenticated, controller.putById);
 router.put("/add_review/:id", controller.addReviewById);
